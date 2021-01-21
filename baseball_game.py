3013bcd86bfda05731270089e30d957369269d38
minyoung1842 <pmin0.1842@gmail.com>
2021-01-21 23:59:37 +0900

    Update my code

--- baseball_game.py
+++ baseball_game.py
@@ -282,7 +282,7 @@ def main():
     # ===Modify codes below=============
     # 위의 코드를 포함하여 자유로운 수정이 가능함
 
-     while True:
+    while True:
         user_input = input("Input guess number: ")
         if user_input == "0": break
             
